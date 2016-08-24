00fbe927d28343f42cf8c27f80641e98f526bdf1
Vallikkannu24 <vallikkannu.13bcs2092@mkce.ac.in>
2016-08-24 14:48:26 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+if(ch=='1'||ch=='2'||ch=='3'||ch=='4'||ch=='5'||ch=='6'||ch=='7'||ch=='8'||ch=='9'||ch=='0')
+{
+printf("not a alphabet");
+}
+else 
+{
+printf("it is a alphabet");
+}
+}
