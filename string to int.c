87463981aea919d60ecbf6d492b97d8a3c8ee1f2
Vallikkannu24 <vallikkannu.13bcs2092@mkce.ac.in>
2016-08-25 10:09:36 +0530

    Create string to int.c

---
+++ string to int.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch[30],ch1[30];
+for(int i=0;i<30;i++)
+{
+scanf("%c",&ch[i]);
+}
+for(int j=0;j<30;j++)
+{
+ch1[i]=printf("%d",ch[i]);
+}
+for(int k=0;k<30;k++)
+{
+printf("%d",ch1[i]);
+}
+}
