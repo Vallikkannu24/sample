a9cc5b69bc6fb72114e9e0bb9ac0edf0dd4f121c
Vallikkannu24 <vallikkannu.13bcs2092@mkce.ac.in>
2016-08-26 10:40:39 +0530

    Update string to int.c

--- string to int.c
+++ string to int.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
 #include<conio.h>
+#include<string.h>
 void main()
 {
-char ch[30],ch1[30];
+char ch[30];
 for(int i=0;i<30;i++)
 {
 scanf("%c",&ch[i]);
 }
-for(int j=0;j<30;j++)
+for(int j=0;j<strlen(ch)-2;j++)
 {
-ch1[i]=printf("%d",ch[i]);
+printf("%d\t",ch[j]);
 }
-for(int k=0;k<30;k++)
-{
-printf("%d",ch1[i]);
-}
+
 }
