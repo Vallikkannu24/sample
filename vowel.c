1b938f6a2879e43409075e903de59e578672dfa7
Vallikkannu24 <vallikkannu.13bcs2092@mkce.ac.in>
2016-08-26 10:45:58 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -3,8 +3,8 @@
 void main()
 {
 char ch;
-scanf("%c",&c);
-if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch='O'||ch='U')
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
 {
 printf("vowel");
 else
