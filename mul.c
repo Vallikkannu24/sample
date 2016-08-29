1c90b31699dc18e317c2c3a22f85469422ef89cf
Vallikkannu24 <vallikkannu.13bcs2092@mkce.ac.in>
2016-08-29 16:06:55 +0530

    Create mul.c

---
+++ mul.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int num,num1,i,mul;
+    scanf("%d",&num);
+    scanf("%d",&num1);
+    for( i=0;i<=num1;i++)
+    {
+        mul=num*i;
+        printf("%d\n",mul);
+    }
+    
+}
