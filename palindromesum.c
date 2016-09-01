463fc4d435f38b9ec4084a6f48ff0c86443482bb
Vallikkannu24 <vallikkannu.13bcs2092@mkce.ac.in>
2016-09-01 06:22:35 +0530

    Update and rename palindrome.c to palindromesum.c

--- palindromesum.c
+++ palindromesum.c
@@ -2,16 +2,24 @@
 #include<conio.h>
 void main()
 {
-int num,temp,temp1,rev=0;
+int num,temp,temp1,rev=0,sum=0;
 scanf("%d",&num);
 temp=num;
 while(temp>0)
 {
 temp1=temp%10;
-rev=rev*10+temp1;
+sum=sum+temp1;
 temp=temp/10;
 }
-if(rev==num)
+temp2=sum;
+while(temp2>0)
+{
+temp3=temp2%10;
+rev=rev*10+temp1;
+temp2=temp2/10;
+}
+
+if(rev==sum)
 {
 printf("the number is palindrome");
 }
